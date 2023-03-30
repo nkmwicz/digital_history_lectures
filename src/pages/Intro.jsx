ea7bd26e2067871cb3b0647e3fe78a621a738ce0
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-30 19:28:36 -0400

    add intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,5 +1,20 @@
+import { QuickLayoutNoArrows } from "@nkmwicz/reactpresentation";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { slideState } from "../components/globalState";
+
 export function Intro() {
-  return (
-    
-  )
+  const [slides, setSlides] = useRecoilState(slideState);
+  const slide = useRecoilValue(currentSlide);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/digital-history/wk01.1.json"
+      );
+      const data = await response.json();
+      setSlides(data);
+    }
+  }, []);
+
+  return <QuickLayoutNoArrows slide={slide} />;
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Intro } from "../pages/Intro";
 
 export function Routes() {
   return (
