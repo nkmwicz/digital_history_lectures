98561bc4af1793ed4b8521802868245a0ceef0f3
Nathan Michalewicz <nathan@mwicz.com>
2023-03-30 17:56:43 -0400

    add reactpresentation styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 import "./index.css";
+import "./@nkmwicz/reactpresentation/dist/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
