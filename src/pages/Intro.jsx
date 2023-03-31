f5bb6185219c11deb2c15ffb293a89cbbe1f590b
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-31 16:09:11 -0400

    add url

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,6 +3,6 @@ import { PageTemplate } from "../components/PageTemplate";
 
 export function Intro() {
   return (
-    <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/digital-history/intro.json" />
+    <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/digital-history/01-intro.json" />
   );
 }
