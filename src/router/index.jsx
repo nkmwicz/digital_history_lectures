4a0b575e31b12e51a26c0b917f7b56724befebac
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-31 16:09:26 -0400

    add route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Intro } from "../pages/Intro";
+import { DigitalHistory } from "../pages/DigitalHistory";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
+      <Route path="/digital-history" element={<DigitalHistory />} />
     </Routes>
   );
 }
