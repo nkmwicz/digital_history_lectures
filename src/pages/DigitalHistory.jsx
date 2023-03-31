42fb88de7c0b414181289a74304ea374465716a9
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-31 16:08:55 -0400

    new component

---
+++ src/pages/DigitalHistory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PageTemplate } from "../components/PageTemplate";
+
+export function DigitalHistory() {
+  return (
+    <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/digital-history/02-digital-history.json" />
+  );
+}
