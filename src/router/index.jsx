9b60b75e65931d77155342f69793e3c082d1fb9c
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-04-10 16:36:56 -0400

    add internet-history

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Intro } from "../pages/Intro";
-import { DigitalHistory } from "../pages/DigitalHistory";
+import { Intro } from "../pages/01Intro";
+import { DigitalHistory } from "../pages/02DigitalHistory";
+import { InternetHistory } from "../pages/03InternetHistory";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
       <Route path="/digital-history" element={<DigitalHistory />} />
+      <Route path="/internet-history" element={<InternetHistory />} />
     </Routes>
   );
 }
