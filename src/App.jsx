d1a8aa5e5a7c79968c0eefb6b5e0cef4d9882c67
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-04-10 16:36:00 -0400

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { Arrow } from "./components/Arrow";
 import { Router } from "./router";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Arrow />
       <Router />
     </>
