a24feabe27a63bbe80bad76866ff8338fe934f0c
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-04-10 16:36:42 -0400

    new component

---
+++ src/pages/03InternetHistory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PageTemplate } from "../components/PageTemplate";
+
+export function InternetHistory() {
+  return (
+    <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/digital-history/03-internet.json" />
+  );
+}
