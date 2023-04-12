f5b8a95cf1b613f99f3a41d6a49104076f8ce159
Nathan Michalewicz <nathan@mwicz.com>
2023-04-12 13:47:05 -0400

    add trad vs digital research

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -3,5 +3,5 @@ export const pages = [
   { title: "Digital History", path: "/digital-history" },
   { title: "Internet History", path: "/internet-history" },
   { title: "New Narratives & SEO", path: "/narratives-and-seo" },
-  { title: "New Narratives & SEO", path: "/narratives-and-seo" },
+  { title: "Traditional vs Digital", path: "/traditional-and-digital" },
 ];
