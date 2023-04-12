406a3ae2871bfb558449e9153a9a4580761000f9
Nathan Michalewicz <nathan@mwicz.com>
2023-04-12 13:42:51 -0400

    add new narratives

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -2,4 +2,6 @@ export const pages = [
   { title: "Intro", path: "/" },
   { title: "Digital History", path: "/digital-history" },
   { title: "Internet History", path: "/internet-history" },
+  { title: "New Narratives & SEO", path: "/narratives-and-seo" },
+  { title: "New Narratives & SEO", path: "/narratives-and-seo" },
 ];
